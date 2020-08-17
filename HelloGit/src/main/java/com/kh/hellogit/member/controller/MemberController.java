f33e37cb596c0ccc9ef4bf60a74dc593d8f1908e
Leehuseok <dlgn96@naver.com>
2020-08-17 10:12:29 +0900

    hh

--- HelloGit/src/main/java/com/kh/hellogit/member/controller/MemberController.java
+++ HelloGit/src/main/java/com/kh/hellogit/member/controller/MemberController.java
@@ -1,5 +1,12 @@
 package com.kh.hellogit.member.controller;
 
-public class MemberController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class MemberController {
+	@RequestMapping("login.do")
+	public String memberLogin(String id, String pwd) {
+		return "home";
+	}
 }
